2feefec0bb13f8d53a9631cb87bd00d9d8a2b36f
victor-floriano <victorgf@bu.edu>
2024-11-06 23:24:23 +0000

    Added prefect scheduling

---
+++ prefect/flows/deploy-elt.py
@@ -0,0 +1,16 @@
+from prefect import flow
+
+if __name__ == "__main__":
+    flow.from_source(
+        source="https://github.com/victor-floriano/ba882-victor-floriano.git",
+        entrypoint="prefect/flows/etl.py:etl_flow",
+    ).deploy(
+        name="aws-blogs-etl",
+        work_pool_name="brock-worker1",
+        job_variables={"env": {"BROCK": "loves-to-code"},
+                       "pip_packages": ["pandas", "requests"]},
+        cron="15 0 * * *",
+        tags=["prod"],
+        description="The pipeline to populate the stage schema with the newest posts.  Version is just for illustration",
+        version="1.0.0",
+    )
